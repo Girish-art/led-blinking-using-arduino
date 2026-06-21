0a1dabb25a8e9bd240d76b792ce4daae1797d41d
Girish-art <gp5992702@gmail.com>
2026-06-21 16:05:35 +0530

    Update led blinking.ino

--- led blinking.ino
+++ led blinking.ino
@@ -1,7 +1,4 @@
-#LED BLINKING USING ARDUINO
-##DESCRIPTION
-This project demonstrates a basic LED blinking circuit using Arduino Uno.
-The LED turns ON for 1 second and OFF for 1 second repeatedly.
+
   ## Arduino Code 
   int led = 13;
 
@@ -18,12 +15,4 @@ void loop()
   digitalWrite(13, LOW);
   delay(1000);
 }
-## Components Used
-- Arduino Uno
-- LED
-- 220 Ohm Resistor
-- Breadboard
-- Jumper Wires
-##Project video
-https://youtu.be/lVV98dBYTg4?si=mejkpYlV_gf9EYZj
-Created by Girish Patil
+
